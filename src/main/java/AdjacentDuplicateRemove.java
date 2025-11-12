3e1ae5152ac060f58ba883eb86794e7d34df0852
Sameer Raj <sameerazzj2@gmail.com>
2025-11-12 22:20:00 +0530

    AdjacentDuplicateRemove

---
+++ src/main/java/AdjacentDuplicateRemove.java
@@ -0,0 +1,19 @@
+public class AdjacentDuplicateRemove {
+
+    public static void main(String[] args) {
+        String s="abbaca";
+
+        StringBuilder sb=new StringBuilder();
+
+        for(char c:s.toCharArray()){
+            int size=sb.length();
+            if(size>0 && sb.charAt(size-1)==c){
+                sb.deleteCharAt(size-1);
+            }
+            else {
+                sb.append(c);
+            }
+        }
+        System.out.println("the final string is "+sb);
+    }
+}
